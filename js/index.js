960f5b37fd45b43185e9f87a80d88871a8aca30c
Thinh Nguyen <dopagovn@gmail.com>
2024-05-02 13:20:25 +0700

    Change everything

--- js/index.js
+++ js/index.js
@@ -1,239 +1,97 @@
 // Chặn những người nhiều chuyện như thằng ranh con Vinh chẳng hạn xem source code chứ cũng chả chặn hoàn toàn được =))).
 // Ai có nhã ý muốn tham khảo có thể ib vào https://facebook.com/dop4go
 
-// $(document).ready(function () {
-//   $(document).on("contextmenu", function (e) {
-//     e.preventDefault();
-//   });
+$(document).ready(function () {
+  $(document).on("contextmenu", function (e) {
+    e.preventDefault();
+  });
 
-//   $(document).on("keydown", function (e) {
-//     // Chặn người dùng nhấn F12 hoặc CTRL+U
-//     if (
-//       (e.key === "F12" && e.ctrlKey === false) ||
-//       (e.ctrlKey === true && e.key === "u")
-//     ) {
-//       e.preventDefault();
-//       alert(
-//         "Độc quyền của thisisquocthinh đấy. Không có xem code được đâu mấy bé!"
-//       );
-//     }
-//   });
-// });
-
-let isPlay = false;
-var icon = $(".pause-play-btn i");
-const playBtn = $(".pause-play-btn");
-const songTitle = $(".song-title");
-const timeControl = $(".time-control");
-const songImage = $(".song-image");
-const music = $(".music");
-const next = $("#next");
-const prev = $("#prev");
-const list = $(".playlist");
-
-
-// handle show dashboard
-
-$(".btn-agree").click(() => {
-  $(".main-hi").removeClass("show");
-  $(".btn-agree").removeClass("show");
-  $(".main-hi").addClass("hide");
-  $(".btn-agree").addClass("hide");
-  setTimeout(() => {
-    $(".main-hi").css("display", "none");
-    $(".btn-agree").css("display", "none");
-  }, 1999)
-  setTimeout(() => {
-    $(".dashboard").removeClass("hide");
-    $(".dashboard").addClass("show");
-    $(".dashboard").css("display", "flex");
-  }, 2001)
-})
-
-const app = {
-  listMusic: [
-    {
-      name: 'A song for you - Joonil Jung',
-      url: '../music/asongforyou.mp3',
-      image: '../image/image1.jpg',
-    },
-    {
-      name: 'A thousand years - Christina Perri',
-      url: '../music/athousandyears.mp3',
-      image: '../image/image2.jpg',
-    },
-    {
-      name: 'Tip Toe - HYBS',
-      url: '../music/tiptoe.mp3',
-      image: '../image/image3.jpg',
-    },
-    {
-      name: 'Ride - HYBS',
-      url: '../music/ride.mp3',
-      image: '../image/image4.jpg',
-    },
-    {
-      name: 'One call away - Charlie Puth',
-      url: '../music/onecallaway.mp3',
-      image: '../image/image5.webp',
-    },
-    {
-      name: 'Patient - Charlie Puth',
-      url: '../music/patient.mp3',
-      image: '../image/image6.webp',
-    },
-    {
-      name: 'Golden hour - JVKE',
-      url: '../music/goldenhour.mp3',
-      image: '../image/image7.jpg',
-    },
-    {
-      name: 'Van Gogh - Dept',
-      url: '../music/vangogh.mp3',
-      image: '../image/image8.jpg',
-    },
-    {
-      name: 'This is what falling in love feels like - Dept',
-      url: '../music/this-is-what-falling-love-feels-like.mp3',
-      image: '../image/image9.jpg',
-    },
-  ],
-  currentIndex: 0,
-
-  defineProperties: function () {
-    Object.defineProperty(this, 'currentSong', {
-      get: function () {
-        return this.listMusic[this.currentIndex];
-      }
-    })
-  },
-
-  loadCurrentSong: function () {
-    songTitle.text(this.currentSong.name);
-    songImage.attr("src", this.currentSong.image);
-    music.attr("src", this.currentSong.url);
-  },
-
-  render: function () {
-    const htmls = this.listMusic.map(song => {
-      return `<div class="song-element">
-      <div class="left-item">
-        <img class="image-list" src="${song.image}" alt="">
-      </div>
-      <div class="right-item">
-        <span>${song.name}</span>
-      </div>
-    </div>`
-    })
-    list.append(htmls);
-  },
-
-  nextSong: function () {
-    this.currentIndex++;
-    if (this.currentIndex >= this.listMusic.length) {
-      this.currentIndex = 0;
+  $(document).on("keydown", function (e) {
+    // Chặn người dùng nhấn F12 hoặc CTRL+U
+    if (
+      (e.key === "F12" && e.ctrlKey === false) ||
+      (e.ctrlKey === true && e.key === "u")
+    ) {
+      e.preventDefault();
+      alert(
+        "Độc quyền của thisisquocthinh đấy. Không có xem code được đâu mấy bé!"
+      );
     }
-    this.loadCurrentSong();
-    music[0].play();
-    music[0].currentTime = 0;
-    timeControl.attr("defaultValue", 0);
-    timeControl.attr("value", 0);
-    timeControl.attr("valueAsNumber", 0);
-  },
+  });
+});
 
-  prevSong: function () {
-    this.currentIndex--;
-    if (this.currentIndex < 0) {
-      this.currentIndex = this.listMusic.length - 1;
+const btnAgree = $(".btn-agree");
+const titleElement = $(".title");
+const buttonContainer = $(".buttons");
+const yesButton = $(".btn--yes");
+const noButton = $(".btn--no");
+const catImg = $(".cat-img");
+
+const MAX_IMAGE = 5;
+let play = true;
+let noCount = 0;
+
+var music = document.getElementsByClassName("music");
+
+btnAgree.on("click", function () {
+  $(".content").toggleClass("fadeIn fadeOut");
+  setTimeout(function () {
+    $(".content").toggleClass("show hidden");
+  }, 1001);
+  music[0].play();
+  setTimeout(function () {
+    $(".valentine").toggleClass("hidden show");
+    $(".valentine").addClass("fadeIn");
+  }, 1002);
+});
+
+function handleYesClick() {
+  titleElement.html("Yayyyy! :3");
+  buttonContainer.addClass("hidden");
+  changeImage("yes");
+}
+
+function handleNoClick() {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGE);
+    changeImage(imageIndex);
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGE) {
+      noCount = 0;
     }
-    this.loadCurrentSong();
-    music[0].play();
-    music[0].currentTime = 0;
-    timeControl.attr("defaultValue", 0);
-    timeControl.attr("value", 0);
-    timeControl.attr("valueAsNumber", 0);
-  },
+  }
+}
 
-  handleEvent: function () {
-    const songImageWidth = songImage.width();
-    $(".playlist").on("scroll", function () {
+function resizeYesButton() {
+  const fontSize = yesButton.css("font-size");
+  const newFontSize = parseFloat(fontSize) * 1.6;
+  console.log(newFontSize);
+  yesButton.css("font-size", `${newFontSize}px`);
+}
 
-      const scrollTop = $(".playlist").scrollTop();
-      const newWidth = songImageWidth - scrollTop;
-      console.log(scrollTop)
+function generateMessage(noCount) {
+  const messages = [
+    "No",
+    "Are you sure?",
+    "Pookie please",
+    "Don't do this to me :(",
+    "You're breaking my heart",
+    "I'm gonna cry...",
+  ];
 
-      newWidth > 0 ? (songImage.css("width", `${newWidth}px`),
-        songImage.css("height", `${newWidth}px`)) : (songImage.css("width", "0px"),
-        songImage.css("height", "0px"))
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
 
-        console.log(newWidth);
+function changeImage(image) {
+  catImg.attr("src", `image/cat-${image}.jpg`);
+}
 
-    })
+function updateNoButtonText() {
+  noButton.html(generateMessage(noCount));
+}
 
-
-    var _this = this;
-    playBtn.click(() => {
-      if (isPlay) {
-        music[0].pause();
-      } else {
-        music[0].play();
-      }
-    })
-
-    music[0].onplay = function () {
-      isPlay = true;
-      icon.removeClass("bi-play-fill").addClass("bi-pause")
-    }
-
-    music[0].onpause = function () {
-      isPlay = false;
-      icon.removeClass("bi-pause").addClass("bi-play-fill")
-    }
-
-    music[0].ontimeupdate = function () {
-      if (music[0].duration) {
-        const progress = Math.floor(music[0].currentTime / music[0].duration * 100);
-        timeControl.attr("value", progress);
-        timeControl.attr("defaultValue", progress);
-        timeControl.attr("valueAsNumber", progress);
-        timeControl.val(progress);
-      }
-    }
-
-    timeControl[0].oninput = function (e) {
-      const seekTime = music[0].duration / 100 * e.target.value;
-      music[0].currentTime = seekTime;
-    }
-
-    next.click(function () {
-      _this.nextSong();
-    })
-
-    prev.click(function () {
-      _this.prevSong();
-    })
-  },
-
-
-
-
-  start: function () {
-    // Định nghĩa các thuộc tính object; 
-    this.defineProperties();
-
-    // Lắng nghe/xử lý sự kiện
-    this.handleEvent();
-
-    // Load bài hát hiện tại 
-    this.loadCurrentSong();
-
-    // Render danh sách nhạc
-    this.render();
-
-  },
-
-
-};
-
-
-app.start();
+yesButton.on("click", handleYesClick);
+noButton.on("click", handleNoClick);
